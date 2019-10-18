b6bfef384206641f7dcc2da054bc4cd2dabbdc89
Janis <janis@MacBook-Pro-de-Janis.local>
2019-10-18 14:15:37 -0500

    2

---
+++ src/app/api.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  END_POINT = 'https://www.metaweather.com/api/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getLocations(queryStr:String){
+    return this.httpClient.get(this.END_POINT + 'location/search/?query=' + queryStr);
+  }
+}
